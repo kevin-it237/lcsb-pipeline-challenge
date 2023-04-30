fb4f557d9093cd46a86932dec5a0b9662bed8984
kevin-it237 <ngaleuabel@gmail.com>
2023-04-30 22:31:37 +0200

    Update test cases

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,6 +1,7 @@
 import unittest
 import os
-from pipeline import load_data, find_min_max, generate_output, generate_csv
+import csv
+from pipeline import load_and_process_data, generate_output, generate_csv
 import pandas as pd
 
 abs_path = os.path.dirname(__file__)
@@ -13,6 +14,7 @@ matrix_3 = [[786,876801,876323], [7653,42781,98261873], [233,4,986542]]
 class TestPipeline(unittest.TestCase):
     def setUp(self):
         """Generate the testing files"""
+        self.results = []
         try:
             os.mkdir(os.path.join(abs_path, "tests")) # create the tests folder
         except OSError as error: 
@@ -28,45 +30,35 @@ class TestPipeline(unittest.TestCase):
             folder_path = os.path.join(abs_path, "{}/{}".format("tests", "test-data-{}.csv".format(str(i+1))))
             generate_csv(m, folder_path)
 
-    def test_load_data(self):
+    def test_load_and_process_data(self):
         """Check that data are loaded correctly"""
-        data = load_data(input_path="tests")
+        data = load_and_process_data(input_path="tests")
         # It should successfully load 3 csv files into a list
         self.assertEqual(len(data), 3)
-        # check the loaded data
-        self.assertEqual(data[0], matrix_1)
-        self.assertEqual(data[1], matrix_2)
-        self.assertEqual(data[2], matrix_3)
-        return data
 
-
-    def test_find_min_max(self):
-        # It should return the max and the min value of each
-        max_min_data = find_min_max(self.test_load_data())
-        self.assertEqual(len(max_min_data), 3)
-        
         # check matrix max/min values
-        self.assertEqual(max_min_data[0], [992729, 232]) # file 1
-        self.assertEqual(max_min_data[1], [8788898, 23]) # file 2
-        self.assertEqual(max_min_data[2], [98261873, 4]) # file 3
-        return max_min_data
+        self.assertEqual(data[0], [992729, 232]) # file 1
+        self.assertEqual(data[1], [8788898, 23]) # file 2
+        self.assertEqual(data[2], [98261873, 4]) # file 3
+        return data
 
     def test_generate_output(self):
         # It should create a two column csv file (tests/output.csv)
-        generate_output(data=self.test_find_min_max(), output_dir="tests")
-        output_dir = os.path.join(abs_path, "tests/output.csv")
+        generate_output(data=self.test_load_and_process_data(), output_dir="tests")
         
         # Check its content
+        output_dir = os.path.join(abs_path, "tests/output.csv")
         df = pd.read_csv(output_dir, header=None)
         output_content = df.values.tolist()
         self.assertEqual(output_content[0], [992729, 232])
         self.assertEqual(output_content[1], [8788898, 23])
         self.assertEqual(output_content[2], [98261873, 4])
 
+
 if __name__ == '__main__':
     #unittest.main()
     # order sequence of execution
-    test_order = ["test_load_data", "test_find_min_max", "test_generate_output"] 
+    test_order = ["test_load_and_process_data", "test_generate_output"] 
     test_loader = unittest.TestLoader()
     test_loader.sortTestMethodsUsing = lambda x, y: test_order.index(x) - test_order.index(y)
     unittest.main(testLoader=test_loader)
